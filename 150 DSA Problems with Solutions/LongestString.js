2f00671463427fda6d381811121ccaf8bfbfb843
Md Sahil Achanoor <117533136+Mahmadsahil@users.noreply.github.com>
2024-07-05 11:31:17 -0800

    Create LongestString.js

---
+++ 150 DSA Problems with Solutions/LongestString.js
@@ -0,0 +1,18 @@
+const longestString = (words) => {
+    let longestStr = "";
+    for (let i = 0; i < words.length; i++) {
+        const currentStr = words[i];
+        if (currentStr.length > longestStr.length) {
+            longestStr = currentStr
+        }
+    }
+    return longestStr
+}
+
+const words1 = ['a', 'ab', 'abc', 'abcd'];
+const words2 = ['apple', 'banana', 'mango', 'pinapple'];
+const words3 = ['car', 'road', 'building', 'city'];
+
+console.log(longestString(words1));
+console.log(longestString(words2));
+console.log(longestString(words3));
