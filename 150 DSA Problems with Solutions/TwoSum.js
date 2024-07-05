e09b3d15adccf935625c88e10fc575dbbb455585
Md Sahil Achanoor <117533136+Mahmadsahil@users.noreply.github.com>
2024-07-05 11:34:41 -0800

    Update TwoSum.js

--- 150 DSA Problems with Solutions/TwoSum.js
+++ 150 DSA Problems with Solutions/TwoSum.js
@@ -15,6 +15,6 @@ const nums1 = [2, 7, 11, 15], target1 = 9
 const nums2 = [3, 2, 4], target2 = 6
 const nums3 = [3, 3], target3 = 6
 
-console.log(TwoSum(nums1, target1));
-console.log(TwoSum(nums2, target2));
-console.log(TwoSum(nums3, target3));
+console.log(TwoSum(nums1, target1));    // [2,1]
+console.log(TwoSum(nums2, target2));    // [2,2]
+console.log(TwoSum(nums3, target3));    // [3,1]
