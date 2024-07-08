13eeb10d0a44d0d8bf8ba21ff59e175d77ea28db
Md Sahil Achanoor <117533136+Mahmadsahil@users.noreply.github.com>
2024-07-08 11:04:27 -0800

    Create RemoveGivenElement.js

---
+++ 150 DSA Problems with Solutions/RemoveGivenElement.js
@@ -0,0 +1,13 @@
+const removeGivenElement = (nums, val) => {
+    let k = 0;
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] !== val) {
+            nums[k] = nums[i];
+            k++
+        }
+    }
+    return k;
+}
+
+const nums = [3, 2, 2, 3], val = 2;
+removeGivenElement(nums, val)
